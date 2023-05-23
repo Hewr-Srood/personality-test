93c243deb0f3a97fcbf6489d9cd65246d8d14dfe
Hewr Srood <Hewr_s@outlook.com>
2023-05-23 07:32:57 +0300

    home route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,9 @@ import { Stack } from 'expo-router';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { store } from '../src/redux/store';
-
+export const unstable_settings = {
+  initialRouteName: 'home/index',
+};
 export default function Layout() {
   return (
     <Provider store={store}>
