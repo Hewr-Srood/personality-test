0d702505e3f6df48318d090c75b671f17d3bfa86
Hewr Srood <Hewr_s@outlook.com>
2023-05-23 07:01:30 +0300

    boilerplate

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  importOrderSeparation: true,
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: "all",
+  jsxSingleQuote: true,
+};
