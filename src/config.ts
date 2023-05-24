d2b89471a211b785b5f701ee79e067095465642c
Hewr Srood <Hewr_s@outlook.com>
2023-05-24 08:22:10 +0300

    modify config

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,9 @@ export const spacing = {
   xs: 4,
   sm: 8,
   md: 16,
-  lg: 24,
-  xl: 40,
+  lg: 20,
+  xl: 24,
+  xxl: 40,
 };
 export const COLORS = {
   primary: '#705771',
@@ -16,9 +17,9 @@ export const COLORS = {
   transparent: 'transparent',
 };
 export const BORDER_RADIUS = {
-  s: 4,
-  m: 10,
-  l: 25,
+  sm: 4,
+  md: 10,
+  lg: 25,
   xl: 75,
 };
 export const { width, height } = Dimensions.get('window');
